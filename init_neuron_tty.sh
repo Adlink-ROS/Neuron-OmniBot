6ee3472545d3928efba661b5468edbce138258ee
Ting Chang <qting0529@hotmail.com>
2020-06-08 15:59:43 +0800

    Update init_neuron_tty.sh

--- init_neuron_tty.sh
+++ init_neuron_tty.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE:="0666", GROUP:="dialout",  SYMLINK+="neurontty"' >/etc/udev/rules.d/neuron_tty.rules
+status=$?
+[ $status -eq 0 ] && echo "Initialized NeuronBot successfully" || echo "Failed to initilize NeuronBot!"; exit -1
 
 service udev reload
 sleep 2
 service udev restart
 
 status=$?
-[ $status -eq 0 ] && echo "Initialized successfully" || echo "Failed to initilized!"
+[ $status -eq 0 ] && echo "Udev restarted successfully." || echo "Failed to restart udev!"
