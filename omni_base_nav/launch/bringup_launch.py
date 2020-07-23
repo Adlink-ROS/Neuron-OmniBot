106eb99c292d2e22cbfc6fb30fb13fa706bf180a
Ting Chang <qting0529@hotmail.com>
2020-07-23 17:05:18 +0800

    Fix bringup_launch.py in omni_base_nav

--- omni_base_nav/launch/bringup_launch.py
+++ omni_base_nav/launch/bringup_launch.py
@@ -17,13 +17,11 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, GroupAction,
-                            IncludeLaunchDescription, SetEnvironmentVariable)
-from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import PushRosNamespace
 
+from nav2_common.launch import Node
 
 def generate_launch_description():
     # Get the launch directory
@@ -37,13 +35,13 @@ def generate_launch_description():
 
     # Create the launch configuration variables
     namespace = LaunchConfiguration('namespace')
-    use_namespace = LaunchConfiguration('use_namespace')
     map_yaml_file = LaunchConfiguration('map')
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
     bt_xml_file = LaunchConfiguration('bt_xml_file')
     autostart = LaunchConfiguration('autostart')
     open_rviz = LaunchConfiguration('open_rviz')
+    use_remappings = LaunchConfiguration('use_remappings')
 
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
@@ -52,11 +50,6 @@ def generate_launch_description():
         'namespace',
         default_value='',
         description='Top-level namespace')
-
-    declare_use_namespace_cmd = DeclareLaunchArgument(
-        'use_namespace',
-        default_value='false',
-        description='Whether to apply a namespace to the navigation stack')
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
@@ -83,43 +76,58 @@ def generate_launch_description():
         default_value='true',
         description='Automatically startup the nav2 stack')
 
+    declare_use_remappings_cmd = DeclareLaunchArgument(
+        'use_remappings',
+        default_value='false',
+        description='Arguments to pass to all nodes launched by the file')
+
     declare_open_rviz_cmd = DeclareLaunchArgument(
         'open_rviz',
         default_value='false',
         description='Launch Rviz?')
 
     # Specify the actions
-    bringup_cmd_group = GroupAction([
-        PushRosNamespace(
-            condition=IfCondition(use_namespace),
-            node_namespace=namespace),
+    start_localization_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'localization_launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'map': map_yaml_file,
+                          'use_sim_time': use_sim_time,
+                          'autostart': autostart,
+                          'params_file': params_file,
+                          'use_lifecycle_mgr': 'false',
+                          'use_remappings': use_remappings}.items())
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'localization_launch.py')),
-            launch_arguments={'namespace': namespace,
-                              'map': map_yaml_file,
-                              'use_sim_time': use_sim_time,
-                              'autostart': autostart,
-                              'params_file': params_file,
-                              'use_lifecycle_mgr': 'false'}.items()),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'navigation_launch.py')),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'autostart': autostart,
-                              'params_file': params_file,
-                              'bt_xml_file': bt_xml_file,
-                              'use_lifecycle_mgr': 'false',
-                              'map_subscribe_transient_local': 'true'}.items()),
+    start_navigation_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'navigation_launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'use_sim_time': use_sim_time,
+                          'autostart': autostart,
+                          'params_file': params_file,
+                          'bt_xml_file': bt_xml_file,
+                          'use_lifecycle_mgr': 'false',
+                          'use_remappings': use_remappings,
+                          'map_subscribe_transient_local': 'true'}.items())
                               
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'rviz_view_launch.py')),
-            launch_arguments={'use_sim_time': use_sim_time,
-                              'open_rviz': open_rviz,
-                              'map_subscribe_transient_local': 'true'}.items()),
+    start_rviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(my_launch_dir, 'rviz_view_launch.py')),
+        launch_arguments={'use_sim_time': use_sim_time,
+                          'open_rviz': open_rviz,
+                          'map_subscribe_transient_local': 'true'}.items())
                              
-    ])
+    start_lifecycle_manager_cmd = Node(
+        package='nav2_lifecycle_manager',
+        node_executable='lifecycle_manager',
+        node_name='lifecycle_manager',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time},
+                    {'autostart': autostart},
+                    {'node_names': ['map_server',
+                                    'amcl',
+                                    'controller_server',
+                                    'planner_server',
+                                    'recoveries_server',
+                                    'bt_navigator',
+                                    'waypoint_follower']}])
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -129,15 +137,18 @@ def generate_launch_description():
 
     # Declare the launch options
     ld.add_action(declare_namespace_cmd)
-    ld.add_action(declare_use_namespace_cmd)
     ld.add_action(declare_map_yaml_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_bt_xml_cmd)
+    ld.add_action(declare_use_remappings_cmd)
     ld.add_action(declare_open_rviz_cmd)
 
     # Add the actions to launch all of the navigation nodes
-    ld.add_action(bringup_cmd_group)
+    ld.add_action(start_lifecycle_manager_cmd)
+    ld.add_action(start_localization_cmd)
+    ld.add_action(start_rviz_cmd)
+    ld.add_action(start_navigation_cmd)
 
     return ld
